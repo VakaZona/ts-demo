32e0b038f1e45f80139c583dd19880ff85af06e1
Valery Zonov <valery.zonov@sibers.com>
2023-02-10 17:08:17 +0700

    initial

---
+++ test.js
@@ -0,0 +1,7 @@
+"use strict";
+let a = 4;
+let b = 'testaaaa';
+let c = true;
+const d = ['1', 'hello', 'my'];
+let e = 3;
+e = 'aff';
---
+++ test.ts
@@ -0,0 +1,8 @@
+let a: number = 4;
+let b: string = 'testaaaa'
+let c = true; 
+
+const d: string[] = ['1', 'hello', 'my']
+
+let e: any = 3
+e = 'aff'
