ebd8c97c66f7a8b02cc67d251efa337f4bf9494b
Valery Zonov <valery.zonov@sibers.com>
2023-02-10 17:22:41 +0700

    Basic type

--- test.ts
+++ test.ts
@@ -2,7 +2,46 @@ let a: number = 4;
 let b: string = 'testaaaa'
 let c = true; 
 
-const d: string[] = ['1', 'hello', 'my']
+let d: string[] = ['1', 'hello', 'my']
 
 let e: any = 3
 e = 'aff'
+
+function testString(a: string): string {
+	return '';
+}
+
+function test(a: string): string | number {
+	return '';
+}
+
+const test2 = (a:  number): number => {
+	return a*2;
+}
+
+d = d.map((x: string) => x.toLowerCase())
+
+function countCoord(coord: { lat: number, long?: number }) {
+	return
+}
+
+function printIt(id: number | string) {
+	if(typeof id === 'number') {
+		console.log(id.toString())
+	} else if (typeof id === 'string') {
+		id.toUpperCase()
+	}
+}
+
+function getSum(a: number | number[]) {
+	if (Array.isArray(a)) {
+		a.map(x => x.toString)
+	}
+}
+
+function returnVoid(a: number): void {
+	return
+}
+
+const x: undefined = undefined
+const z: null = null
