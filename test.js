56a1490b480eaf258b8ca213084ed30dcfca817b
Valery Zonov <valery.zonov@sibers.com>
2023-02-13 12:35:22 +0700

    JSX

--- test.js
+++ test.js
@@ -1,2 +1,9 @@
 "use strict";
-const a = [0, "a", 1];
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const react_1 = __importDefault(require("react"));
+const a = react_1.default.createElement("div", { tabIndex: 0 },
+    react_1.default.createElement("span", null, 1 + 1));
+const b = react_1.default.createElement('div', { tabIndex: 1 + 1 }, 'a');
