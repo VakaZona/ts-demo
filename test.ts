40bd9b19f9ad027624e08fedd283d8089babebc8
Valery Zonov <valery.zonov@sibers.com>
2023-02-13 12:14:21 +0700

    Tuple

--- test.ts
+++ test.ts
@@ -1,57 +1,17 @@
-enum Direction {
-  Up = 1,
-  Down,
-  Left,
-  Right,
-}
+const a: [number, string, number] = [0, "a", 1];
 
-enum DirectionString {
-  Up = "UP",
-  Down = "DOWN",
-  Left = "LEFT",
-  Right = "RIGHT",
-}
-
-enum Decision {
-  Yes = 1,
-  No = calcEnum(),
-}
-
-function calcEnum() {
-  return 2;
-}
-
-function runEnum(obj: { Up: string }) {}
-
-runEnum(DirectionString);
-
-enum Test {
-  A,
-}
-
-let test = Test.A;
-let nameA = Test[test]; // A
-
-const enum ConstEnum {
-  A,
-  B,
-}
-
-let c = ConstEnum.A;
-
-enum Dice {
-  One = 1,
-  Two = 2,
-  Tree,
-}
-
-function ruDice(dice: Dice) {
-  switch (dice) {
-    case Dice.One:
-      return "One";
-    case Dice.Two:
-      return "Two";
-    default:
-      const a: never = dice;
+a.push(1);
+a.map((s) => {
+  switch (typeof s) {
+    case "string":
+      return "string";
+    case "number":
+      return "number";
   }
-}
+});
+
+const [c, d, f] = a;
+
+const [c1, ...rest] = a;
+
+a[1].charAt;
